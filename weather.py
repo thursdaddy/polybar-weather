9bb482d9a717a53a6f1ff9605273bd9fce93a44c
jacob <thursdaddy@gmail.com>
2019-03-25 22:20:33 -0700

    refactored functions and their execution, created basic cache file check function

--- weather.py
+++ weather.py
@@ -3,7 +3,14 @@
 import argparse
 from configparser import ConfigParser
 import logging
+import json
+import requests
 import os
+
+cp = ConfigParser()
+conf_path = os.environ.get("HOME") + "/.config/polybar/scripts/"
+conf_file = conf_path + "py_scripts.conf"
+cache_file = conf_path + "py_weather.cache"
 
 def arg_parser():
     parser = argparse.ArgumentParser()
@@ -11,34 +18,60 @@ def arg_parser():
     args = parser.parse_args()
     return args
 
-# checks for config file, creates if missing
-def conf_checker():
-    conf_path = os.environ.get("HOME") + "/.config/polybar/scripts/"
-    conf_file = conf_path + "py_scripts.conf"
-    cache_file = conf_path + "py_weather.cache"
-    if not os.path.exists(conf_path):
-        os.makedirs(conf_path)
+def conf_creator():
+    os.makedirs(conf_path)
     if not os.path.exists(conf_file):
         cp.add_section('weather')
-        cp.set('weather', 'use_geoloc', '0')
+        cp.set('weather', 'use_geoloc', '1')
         cp.set('weather', 'zipcode', '10001')
         cp.set('weather', 'cache_ageout', '900')
         cp.set('weather', 'forecast_type', 'short')
         with open(conf_file, 'w') as configfile:
             cp.write(configfile)
-        logging.debug("writing: " + conf_file)
+        logging.debug("WRITING: " + conf_file)
+
+def cache_check(cache_file):
+    if os.path.exists(cache_file):
+        print("cache exists")
     else:
-        logging.debug("exists: " + conf_path)
-        pass
+        print("cache does not exist")
 
-# check age of cache file to determine if updated is needed
-def cache_age(cache_file):
-    pass
+def conf_parser(config):
+    cp.read(config)
+    config = cp._sections["weather"]
+    return config
 
-if __name__ == '__main__':
-    args = arg_parser()
-    cp = ConfigParser()
-    if args.verbose:
-        logging.getLogger().setLevel(logging.DEBUG)
-        logging.debug("---VERBOSE LOGGING ENABLED---")
-    conf_checker()
+def get_location():
+    location = requests.get('https://ipinfo.io/json')
+    json = location.json()
+    location = json['loc']
+    return get_forecast_url(location)
+
+def get_forecast_url(location):
+    logging.debug("WEATHER.GOV ENTRY URL: https://api.weather.gov/points/%s" % location)    
+    weather_api = ("https://api.weather.gov/points/%s" % location)
+    response = requests.get(weather_api)
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        logging.debug(e)
+        return print("API DOWN")
+    get_forecast = requests.get(weather_api)
+    json = get_forecast.json()
+    forecast_url = json["properties"]["forecast"]
+    logging.debug("WEATHER.GOV FORECAST URL: " + forecast_url)    
+    return forecast_url
+
+args = arg_parser()
+if args.verbose:
+    logging.getLogger().setLevel(logging.DEBUG)
+    logging.debug("---VERBOSE LOGGING ENABLED---")
+if not os.path.exists(conf_path):
+        conf_creator()
+cache_check(cache_file)
+
+# if cache exists..
+    # check age
+# parse config
+# else check geolocation
+# get weather URL
