7cfc25b6daac044902ef2f2f5fb72c7d94ab607c
jacob <thursdaddy@gmail.com>
2019-03-26 01:57:04 -0700

    while True loop, check if cache age is over cache_ageout value, check conf_file to updates

--- weather.py
+++ weather.py
@@ -21,7 +21,7 @@ def conf_creator():
     os.makedirs(conf_path)
     if not os.path.exists(conf_file):
         cp.add_section('weather')
-        cp.set('weather', 'use_geoloc', '1')
+        cp.set('weather', 'use_geoloc', 'True')
         cp.set('weather', 'zipcode', '10001')
         cp.set('weather', 'cache_ageout', '900')
         cp.set('weather', 'forecast_type', 'short')
@@ -29,21 +29,33 @@ def conf_creator():
             cp.write(configfile)
         logging.debug("WRITING: " + conf_file)
 
+# checks if cache is older than cache_ageout value
+# if conf file is newer than cache it will get weather
 def cache_new(cache_file, cache_ageout):
     import time
+    if not os.path.exists(cache_file):
+        with open(cache_file, 'a'):
+            os.utime(cache_file, None)
     if os.path.exists(cache_file):
-        mod_time = int(os.stat(cache_file).st_mtime)
+        conf_mod_time = int(os.stat(conf_file).st_mtime)
+        cache_mod_time = int(os.stat(cache_file).st_mtime)
         current_time = int(time.time())
-        cache_age = current_time - mod_time
-        logging.debug("Mod Time: " + str(mod_time))
+        cache_age = current_time - cache_mod_time
+        conf_age = current_time -  conf_mod_time
+        logging.debug("Mod Time: " + str(cache_mod_time))
         logging.debug("Current Time: " + str(current_time))
+        logging.debug("Ageout value: " + str(cache_ageout))
         logging.debug("Cache Age: " + str(cache_age))
+        logging.debug("Conf Age: " + str(conf_age))
+        if (conf_age < cache_age):
+            logging.debug("Cache is new OR Conf has been updated!")
+            return(False)
         if cache_age > int(cache_ageout):
             logging.debug("Cache is old")
             return(False)
         else:
             logging.debug("Cache is new")
-            return(False)
+            return(True)
 
 def conf_parser(config_file):
     cp.read(config_file)
@@ -57,10 +69,10 @@ def get_geolocation():
     location = json['loc']
     return get_forecast_url(location)
 
-def get_location(zcode):
+def get_location(zipcode):
     from uszipcode import SearchEngine
     search = SearchEngine()
-    zipcode = search.by_zipcode(zcode)
+    zipcode = search.by_zipcode(zipcode)
     zipcode = zipcode.to_dict()
     lat = zipcode['lat']
     lng = zipcode['lng']
@@ -86,6 +98,10 @@ def get_forecast_url(location):
     logging.debug("WEATHER.GOV FORECAST LOCATION: " + city + "," + state)    
     return forecast_url
 
+def get_forecast(forecast_url):
+
+    return forecast_url
+
 cp = ConfigParser()
 args = arg_parser()
 if args.verbose:
@@ -93,17 +109,21 @@ if args.verbose:
     logging.debug("---VERBOSE LOGGING ENABLED---")
 if not os.path.exists(conf_path):
         conf_creator()
+
+
 config = conf_parser(conf_file)
+use_geoloc = cp.getboolean('weather', 'use_geoloc')
+zipcode = config['zipcode']
+cache_ageout = config['cache_ageout']
+forecast_type = config['forecast_type']
 
 while True:
-    use_geoloc = config['use_geoloc']
-    zcode = config['zipcode']
-    if cache_new(cache_file, config['cache_ageout']) == False:
-        if use_geoloc  == '1':
-            forecast_url = get_geolocation()
-            print(forecast_url)
-            break
-        else: 
-            forecast_url = get_location(zcode)
-            print(forecast_url)
-            break
+    if cache_new(cache_file, cache_ageout):
+        break
+    if use_geoloc:
+        forecast_url = get_geolocation()
+    else: 
+        forecast_url = get_location(zipcode) 
+    forecast = get_forecast(forecast_url)
+    print(forecast)
+    break
