2da08d7f63e6f5280ada067b88a03709e9ec0847
jacob <thursdaddy@gmail.com>
2019-03-27 15:00:33 -0700

    fixed file mod time calculator

--- weather.py
+++ weather.py
@@ -28,64 +28,37 @@ def conf_creator():
             cp.write(configfile)
         logging.debug("WRITING: " + conf_file)
 
-# checks if cache is older than cache_ageout value
-# if conf file is newer than cache it will get weather
-def cache_new(cache_file, cache_ageout):
-    import time
-    if not os.path.exists(cache_file):
-        with open(cache_file, 'a'):
-            os.utime(cache_file, None)
-    if os.path.exists(cache_file):
-        conf_mod_time = int(os.stat(conf_file).st_mtime)
-        cache_mod_time = int(os.stat(cache_file).st_mtime)
-        current_time = int(time.time())
-        cache_age = current_time - cache_mod_time
-        conf_age = current_time -  conf_mod_time
-        logging.debug("Mod Time: " + str(cache_mod_time))
-        logging.debug("Current Time: " + str(current_time))
-        logging.debug("Ageout value: " + str(cache_ageout))
-        logging.debug("Cache Age: " + str(cache_age))
-        logging.debug("Conf Age: " + str(conf_age))
-        if (conf_age < cache_age) or str(conf_age) == "0":
-            logging.debug("Cache is new OR Conf has been updated!")
-            return(False)
-        if cache_age > int(cache_ageout):
-            logging.debug("Cache is old")
-            return(False)
-        else:
-            logging.debug("Cache is new")
-            return(True)
+def debug_config():
+    logging.debug("---START CONFIG---")
+    logging.debug("conf_file: " + conf_file)
+    logging.debug("cache_file: " + cache_file)
+    logging.debug("zipcode: " + zipcode)
+    logging.debug("cache_ageout: " + cache_ageout)
+    logging.debug("forecat_type: " + forecast_type)
+    if use_geoloc:
+        logging.debug("use_geoloc: true")
+    else:
+        logging.debug("use_geoloc: false")
+    logging.debug("---END CONFIG---")
 
 def conf_parser(config_file):
     cp.read(config_file)
     config = cp._sections["weather"]
     return config
 
-def get_geolocation():
-    logging.debug("---GEOLOC ENABLED---")    
-    location = requests.get('https://ipinfo.io/json')
-    json = location.json()
-    location = json['loc']
-    return forecast_get_url(location)
-
-def get_location(zipcode):
-    from uszipcode import SearchEngine
-    search = SearchEngine()
-    zipcode = search.by_zipcode(zipcode)
-    zipcode = zipcode.to_dict()
-    latlong = zipcode['lat'],zipcode['lng']
-    location = ("{0[0]},{0[1]}").format(latlong)
-    return forecast_get_url(location)
-
-def forecast_url_reponse(forecast_url):
-    response = requests.get(forecast_url)
-    try:
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as e:
-        logging.debug(e)
-        return(False)
+def forecast_location(use_geoloc, zipcode):
+    if use_geoloc:
+        location = requests.get('https://ipinfo.io/json')
+        json = location.json()
+        location = json['loc']
     else:
-        return forecast_url
+        from uszipcode import SearchEngine
+        search = SearchEngine()
+        zipcode = search.by_zipcode(zipcode)
+        zipcode = zipcode.to_dict()
+        latlong = zipcode['lat'],zipcode['lng']
+        location = ("{0[0]},{0[1]}").format(latlong)
+    return forecast_get_url(location)
 
 def forecast_get_url(location):
     logging.debug("WEATHER.GOV ENTRY URL: https://api.weather.gov/points/%s" % location)    
@@ -101,6 +74,16 @@ def forecast_get_url(location):
         else: 
             return(False)
 
+def forecast_url_reponse(forecast_url):
+    response = requests.get(forecast_url)
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        logging.debug(e)
+        return(False)
+    else:
+        return(True)
+
 def forecast_type_toggle(forecast_type):
     if forecast_type == "short":
         cp.set('weather', 'forecast_type', 'long')
@@ -108,6 +91,26 @@ def forecast_type_toggle(forecast_type):
         cp.set('weather', 'forecast_type', 'short')
     with open(conf_file, 'w') as conf:
         cp.write(conf)
+
+def forecast_refresh(cache_ageout):
+    import time
+    current_time = int(time.time())
+    cache_mod = int(os.stat(cache_file).st_mtime)
+    conf_mod = int(os.stat(conf_file).st_mtime)
+    logging.debug("CURRENT TIME: " + str(current_time))
+    logging.debug("CONF AGE: " + str(current_time - conf_mod))
+    logging.debug("CACHE AGE: " + str(current_time - cache_mod))
+    if (current_time - conf_mod) == "0":
+        logging.debug("Confile file is NEW! REFRESHING")
+        logging.debug(conf + "is 0")
+    elif (current_time - conf_mod) < (current_time - cache_mod):
+        logging.debug("Conf file is newer than Cache file!! REFRESHING ")
+    elif (current_time - cache_mod) > int(cache_ageout):
+        logging.debug("Cache file is older than " + cache_ageout + "!! REFRESHING ")
+    elif (current_time - cache_mod) < int(cache_ageout):
+        logging.debug("Cache file is newer than " + cache_ageout + " EXITING")
+        return(False)
+    return(True)
 
 def forecast_get_json(forecast_url):
     forecast = requests.get(forecast_url)
@@ -148,22 +151,21 @@ forecast_type = config['forecast_type']
 cache_ageout = config['cache_ageout']
 zipcode = config['zipcode']
 
-## pre-checks
-if args.verbose:
-    logging.getLogger().setLevel(logging.DEBUG)
-    logging.debug("---VERBOSE LOGGING ENABLED---")
-if not os.path.exists(conf_path):
-        conf_creator()
+## get conf_age and cache_age
 
 while True:
+    if args.verbose:
+        logging.getLogger().setLevel(logging.DEBUG)
+        debug_config()
+    if not os.path.exists(conf_path):
+        conf_creator()
     if args.toggle_forecast_type:
         forecast_type_toggle(forecast_type)
-    if cache_new(cache_file, cache_ageout):
+    if forecast_refresh(cache_ageout):
         break
-    if use_geoloc:
-        forecast_url = get_geolocation()
-    else: 
-        forecast_url = get_location(zipcode) 
+        forecast_url = forecast_location(zipcode, use_geoloc)
+    else:
+        break
     if args.notify_5day_forecast or forecast_type == "long":
         forecast_json = forecast_get_json(forecast_url)
         if args.notify_5day_forecast:
